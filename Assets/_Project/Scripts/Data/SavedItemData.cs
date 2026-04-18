c60a1021fb4c0054bc52893a8dca47e8c34dd687
MHowlett1562 <159480506+MHowlett1562@users.noreply.github.com>
2026-04-17 21:12:07 -0400

    Add SavedItemData model

---
+++ Assets/_Project/Scripts/Data/SavedItemData.cs
@@ -0,0 +1,10 @@
+using UnityEngine;
+
+[System.Serializable]
+public class SavedItemData
+{
+	public string itemId;
+	public string itemName;
+	public Vector3 lastKnownPosition;
+	public string savedAtUtc;
+}
